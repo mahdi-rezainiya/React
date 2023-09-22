64269ae7fac4410b4e3c33d84bf03383006a04a2
mahdi rezainiya <mahdi.rezainiya@gmail.com>
2023-09-22 11:24:24 +0330

    New Changes

--- fetching/src/App.js
+++ fetching/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import TripList from './components/TripList';
 
--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+
 import './App.css';
 import React , {useState} from "react";
 import Title from './components/Title';
